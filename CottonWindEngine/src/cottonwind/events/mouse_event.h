795a3b77a64a822c77aad6e4161cd5f19897aa73
Daniel Blagy <daniel.blagy@gmail.com>
2021-01-16 20:24:10 +0300

    added mouse event classes

--- CottonWindEngine/src/cottonwind/events/mouse_event.h
+++ CottonWindEngine/src/cottonwind/events/mouse_event.h
@@ -12,10 +12,45 @@ namespace cotwin
 	{
 	public:
 		Vector2 cursor_position;
+
+	protected:
+		MouseEvent(Vector2 s_cursor_position)
+			: cursor_position(s_cursor_position)
+		{}
+	};
+
+	class MouseMovedEvent : public MouseEvent
+	{
+	public:
+		Vector2 relative_motion;
+
+	public:
+		MouseMovedEvent(Vector2 s_cursor_position, Vector2 s_relative_motion)
+			: MouseEvent(s_cursor_position), relative_motion(s_relative_motion)
+		{}
+	};
+
+	class MouseButtonEvent : public MouseEvent
+	{
+	public:
 		unsigned int button_code;
-		const char* button_name;
+
+	public:
+		MouseButtonEvent(Vector2 s_cursor_position, unsigned int s_button_code)
+			: MouseEvent(s_cursor_position), button_code(s_button_code)
+		{}
+	};
+
+	class MouseWheelEvent : public MouseEvent
+	{
+	public:
 		// x is the amount scrolled horizontally, positive to the right and negative to the left
 		// y is the amount scrolled vertically, positive away from the user and negative towards the user
 		Vector2 wheel_scroll;
+
+	public:
+		MouseWheelEvent(Vector2 s_cursor_position, Vector2 s_wheel_scroll)
+			: MouseEvent(s_cursor_position), wheel_scroll(s_wheel_scroll)
+		{}
 	};
 }
